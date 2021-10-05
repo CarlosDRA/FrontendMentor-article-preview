9a764e6f878dd05c1f2efa6352fb6a894db50d7a
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2021-10-05 16:40:14 -0500

    Finished share arrow clicked style

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,14 @@
 const mediaButton = document.querySelector(".profile__media");
 const mediaSection = document.querySelector(".media__card");
 const mediaSocial = document.querySelector(".media__share");
+const arrow = document.querySelector("#arrow")
 
 document.addEventListener("click", (e)=>{
     if(mediaSection.classList[1] != "hidden" && e.target != mediaSocial 
         && e.target != document.querySelector(".media__text")){
         mediaSection.classList.add("hidden");
         mediaButton.classList.remove("dark__bg");
+        arrow.classList.remove("clear__arrow");
     }
 })
 
@@ -15,4 +17,5 @@ mediaButton.addEventListener("click", (e) =>{
     e.stopPropagation();
     mediaSection.classList.toggle("hidden");
     mediaButton.classList.toggle("dark__bg");
+    arrow.classList.toggle("clear__arrow");
 })
